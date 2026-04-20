b66923caa9f92f97c834adb72130816ee723f805
Saswata777 <saswatamaitra3.14@gmail.com>
2026-04-20 23:44:32 +0530

    feat(core): add lead repository

--- src/main/java/com/freelance/agency/repository/LeadRepository.java
+++ src/main/java/com/freelance/agency/repository/LeadRepository.java
@@ -1,4 +1,19 @@
 package com.freelance.agency.repository;
 
-public class LeadRepository {
+import com.freelance.agency.entity.Lead;
+import com.freelance.agency.enums.LeadStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface LeadRepository extends JpaRepository<Lead, Long> {
+
+    Optional<Lead> findByEmail(String email);
+
+    List<Lead> findByStatus(LeadStatus status);
+
+    boolean existsByEmail(String email);
 }
