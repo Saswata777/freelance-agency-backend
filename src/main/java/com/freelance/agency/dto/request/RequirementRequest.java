4f26f78ec4088e6b6322bbb64f466d777a831182
Saswata777 <saswatamaitra3.14@gmail.com>
2026-04-20 23:52:33 +0530

    feat(chat): add requirement request and analysis response DTOs

--- src/main/java/com/freelance/agency/dto/request/RequirementRequest.java
+++ src/main/java/com/freelance/agency/dto/request/RequirementRequest.java
@@ -1,4 +1,17 @@
 package com.freelance.agency.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class RequirementRequest {
+
+    @NotBlank(message = "Message cannot be empty")
+    private String message;
+
+    @Email(message = "Invalid email format")
+    private String email;
+
+    private String name;
 }
--- src/main/java/com/freelance/agency/dto/response/AnalysisResponse.java
+++ src/main/java/com/freelance/agency/dto/response/AnalysisResponse.java
@@ -1,4 +1,20 @@
 package com.freelance.agency.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AnalysisResponse {
+
+    private String intent;
+    private String message;
+    private boolean matched;
+    private List<ProjectResponse> matchedProjects;
 }
