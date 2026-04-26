c3d054ef434cc772f699d598b15d7477bfda8d0d
Saswata777 <saswatamaitra3.14@gmail.com>
2026-04-26 17:37:52 +0530

    feat(controller): add chat, project, booking, payment, and testimonial controllers

--- src/main/java/com/freelance/agency/controller/ProjectController.java
+++ src/main/java/com/freelance/agency/controller/ProjectController.java
@@ -1,4 +1,30 @@
 package com.freelance.agency.controller;
 
+import com.freelance.agency.constants.AppConstants;
+import com.freelance.agency.dto.response.ProjectResponse;
+import com.freelance.agency.service.ProjectService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(AppConstants.API_PROJECTS)
+@RequiredArgsConstructor
+@Slf4j
 public class ProjectController {
+
+    private final ProjectService projectService;
+
+    @GetMapping
+    public ResponseEntity<List<ProjectResponse>> getAllProjects() {
+        return ResponseEntity.ok(projectService.getAllActiveProjects());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProjectResponse> getProjectById(@PathVariable Long id) {
+        return ResponseEntity.ok(projectService.getProjectById(id));
+    }
 }
